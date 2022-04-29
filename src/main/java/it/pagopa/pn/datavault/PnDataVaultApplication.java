c98c4487bcaf41333c09b8d5e5277bf53420c3e6
marco-vit-pagopa <marco.vit@pagopa.it>
2022-04-29 07:56:12 +0200

    Correzione path / per load balancer

--- src/main/java/it/pagopa/pn/datavault/PnDataVaultApplication.java
+++ src/main/java/it/pagopa/pn/datavault/PnDataVaultApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.result.view.RedirectView;
 
 @PnSpringBootApplication
@@ -15,7 +16,7 @@ public class PnDataVaultApplication {
 	}
 
 
-	@Controller
+	@RestController
 	@RequestMapping("/")
 	public static class RootController {
 

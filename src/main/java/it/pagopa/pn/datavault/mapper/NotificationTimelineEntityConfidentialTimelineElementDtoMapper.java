88215498e50c1fa02412c8ab06449c5f893e4705
Simone Rondinella <simone.rondinella@pagopa.it>
2022-05-23 19:09:41 +0200

    Check null subclass

--- src/main/java/it/pagopa/pn/datavault/mapper/NotificationTimelineEntityConfidentialTimelineElementDtoMapper.java
+++ src/main/java/it/pagopa/pn/datavault/mapper/NotificationTimelineEntityConfidentialTimelineElementDtoMapper.java
@@ -20,8 +20,12 @@ public class NotificationTimelineEntityConfidentialTimelineElementDtoMapper  ext
         final NotificationTimelineEntity target = new NotificationTimelineEntity();
         target.setTimelineElementId(dto.getTimelineElementId());
         target.setDigitalAddress(dto.getDigitalAddress().getValue());
-        target.setPhysicalAddress(toPhysicalAddress(dto.getPhysicalAddress()));
-        target.setNewPhysicalAddress(toPhysicalAddress(dto.getNewPhysicalAddress()));
+        if(dto.getPhysicalAddress() != null){
+            target.setPhysicalAddress(toPhysicalAddress(dto.getPhysicalAddress()));
+        }
+        if(dto.getNewPhysicalAddress() != null){
+            target.setNewPhysicalAddress(toPhysicalAddress(dto.getNewPhysicalAddress()));
+        }
         return target;
     }
 
@@ -32,8 +36,12 @@ public class NotificationTimelineEntityConfidentialTimelineElementDtoMapper  ext
         AddressDto addressDto = new AddressDto();
         addressDto.setValue(entity.getDigitalAddress());
         target.setDigitalAddress(addressDto);
-        target.setPhysicalAddress(toAnalogDomicile(entity.getPhysicalAddress()));
-        target.setNewPhysicalAddress(toAnalogDomicile(entity.getNewPhysicalAddress()));
+        if(entity.getPhysicalAddress() != null){
+            target.setPhysicalAddress(toAnalogDomicile(entity.getPhysicalAddress()));
+        }
+        if(entity.getNewPhysicalAddress() != null){
+            target.setNewPhysicalAddress(toAnalogDomicile(entity.getNewPhysicalAddress()));
+        }
         return target;
     }
 }

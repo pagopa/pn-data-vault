baeff72f9128c0abae381aa24c88e1666916c04b
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-09-01 11:37:29 +0200

    PN-1950: rework exceptions

--- src/main/java/it/pagopa/pn/datavault/exceptions/PnDatavaultExceptionCodes.java
+++ src/main/java/it/pagopa/pn/datavault/exceptions/PnDatavaultExceptionCodes.java
@@ -4,10 +4,6 @@ import it.pagopa.pn.commons.exceptions.PnExceptionsCodes;
 
 public class PnDatavaultExceptionCodes extends PnExceptionsCodes {
 
-    // raccolgo qui tutti i codici di errore delle deleghe
+    // raccolgo qui tutti i codici di errore di datavault
     public static final String ERROR_CODE_DATAVAULT_RECIPIENT_NOT_FOUND = "PN_DATAVAULT_RECIPIENTNOTFOUND";
-    public static final String ERROR_CODE_MANDATE_ALREADY_EXISTS = "PN_MANDATE_ALREADYEXISTS";
-    public static final String ERROR_CODE_MANDATE_NOTACCEPTABLE = "PN_MANDATE_NOTACCEPTABLE";
-    public static final String ERROR_CODE_MANDATE_DELEGATE_HIMSELF = "PN_MANDATE_DELEGATEHIMSELF";
-    public static final String ERROR_CODE_INVALID_VERIFICATION_CODE = "PN_MANDATE_INVALIDVERIFICATIONCODE";
 }

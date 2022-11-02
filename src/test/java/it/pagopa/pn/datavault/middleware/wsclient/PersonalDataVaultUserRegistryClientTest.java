77fec708f9d455606ffc8c951d68bd69ae058d9a
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-11-02 13:37:44 +0100

    PN-2506 aggiunto test retry

--- src/test/java/it/pagopa/pn/datavault/middleware/wsclient/PersonalDataVaultUserRegistryClientTest.java
+++ src/test/java/it/pagopa/pn/datavault/middleware/wsclient/PersonalDataVaultUserRegistryClientTest.java
@@ -2,10 +2,7 @@ package it.pagopa.pn.datavault.middleware.wsclient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Maps;
 import it.pagopa.pn.datavault.generated.openapi.server.v1.dto.BaseRecipientDto;
-import it.pagopa.pn.datavault.generated.openapi.server.v1.dto.RecipientType;
-import it.pagopa.pn.datavault.mandate.microservice.msclient.generated.tokenizer.v1.dto.TokenResourceDto;
 import it.pagopa.pn.datavault.mandate.microservice.msclient.generated.userregistry.v1.dto.CertifiableFieldResourceOfstringDto;
 import it.pagopa.pn.datavault.mandate.microservice.msclient.generated.userregistry.v1.dto.UserResourceDto;
 import org.junit.jupiter.api.AfterAll;
@@ -14,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
+import org.mockserver.matchers.Times;
 import org.mockserver.model.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -194,6 +193,95 @@ class PersonalDataVaultUserRegistryClientTest {
         assertEquals(surname, result.get(0).getDenomination());
         assertEquals(fc, result.get(0).getTaxId());
         assertEquals(expectediuid, result.get(0).getInternalId());
+
+    }
+
+    @Test
+    void getRecipientDenominationByInternalIdPFAfterRetryableException() throws JsonProcessingException {
+        //Given
+        String name = "mario";
+        String surname = "rossi";
+        String fc = "RSSMRA85T10A562S";
+        String iuid = "a8bdb303-18c0-43dd-b832-ef9f451bfe22";
+        String expectediuid = "PF-"+iuid;
+        List<String> ids = Arrays.asList(expectediuid);
+        UserResourceDto response = new UserResourceDto();
+        CertifiableFieldResourceOfstringDto certifiableFieldResourceOfstringDto = new CertifiableFieldResourceOfstringDto();
+        certifiableFieldResourceOfstringDto.setCertification(CertifiableFieldResourceOfstringDto.CertificationEnum.NONE);
+        certifiableFieldResourceOfstringDto.setValue(name);
+        response.setName(certifiableFieldResourceOfstringDto);
+        certifiableFieldResourceOfstringDto = new CertifiableFieldResourceOfstringDto();
+        certifiableFieldResourceOfstringDto.setCertification(CertifiableFieldResourceOfstringDto.CertificationEnum.NONE);
+        certifiableFieldResourceOfstringDto.setValue(surname);
+        response.setFamilyName(certifiableFieldResourceOfstringDto);
+        response.setFiscalCode(fc);
+        response.setId(UUID.fromString(iuid));
+        ObjectMapper mapper = new ObjectMapper();
+        String respjson = mapper.writeValueAsString(response);
+
+
+
+        MockServerClient mockServerClient = new MockServerClient("localhost", 9999);
+        mockServerClient
+                .when(request()
+                        .withMethod("GET")
+                        .withHeader("x-api-key", "pf")
+                        .withQueryStringParameters(Map.of("fl", Arrays.asList("familyName", "name", "fiscalCode")))
+                        .withPath("/users/" + iuid), Times.exactly(1))
+                .respond(response()
+//                        .withBody(respjson)
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withStatusCode(429)
+                );
+
+        mockServerClient
+                .when(request()
+                        .withMethod("GET")
+                        .withHeader("x-api-key", "pf")
+                        .withQueryStringParameters(Map.of("fl", Arrays.asList("familyName", "name", "fiscalCode")))
+                        .withPath("/users/" + iuid))
+                .respond(response()
+                        .withBody(respjson)
+                                .withBody(respjson)
+                                .withContentType(MediaType.APPLICATION_JSON)
+                                .withStatusCode(200)
+                );
+
+        //When
+        List<BaseRecipientDto> result = client.getRecipientDenominationByInternalId(ids).collectList().block();
+
+        //Then
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(name + " " + surname, result.get(0).getDenomination());
+        assertEquals(fc, result.get(0).getTaxId());
+        assertEquals(expectediuid, result.get(0).getInternalId());
+
+    }
+
+    @Test
+    void getRecipientDenominationByInternalIdPFAfterNotRetryableException() {
+        //Given
+        String iuid = "a8bdb303-18c0-43dd-b832-ef9f451bfe22";
+        String expectediuid = "PF-"+iuid;
+        List<String> ids = List.of(expectediuid);
+
+        MockServerClient mockServerClient = new MockServerClient("localhost", 9999);
+        mockServerClient
+                .when(request()
+                        .withMethod("GET")
+                        .withHeader("x-api-key", "pf")
+                        .withQueryStringParameters(Map.of("fl", Arrays.asList("familyName", "name", "fiscalCode")))
+                        .withPath("/users/" + iuid), Times.exactly(1))
+                .respond(response()
+                                .withContentType(MediaType.APPLICATION_JSON)
+                                .withStatusCode(500)
+                );
+
+       assertThrows(
+               WebClientResponseException.InternalServerError.class,
+               () -> client.getRecipientDenominationByInternalId(ids).collectList().block()
+       );
 
     }
 }

0c6652f58f202bea13a1fdec10a8b6c86b048c6c
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-11-10 16:08:42 +0100

    PN-2555 modificata nome immagine per testcontainers

--- src/test/java/it/pagopa/pn/datavault/LocalStackTestConfig.java
+++ src/test/java/it/pagopa/pn/datavault/LocalStackTestConfig.java
@@ -22,7 +22,7 @@ import static org.testcontainers.containers.localstack.LocalStackContainer.Servi
 public class LocalStackTestConfig {
 
     static LocalStackContainer localStack =
-            new LocalStackContainer(DockerImageName.parse(getImageName()).asCompatibleSubstituteFor("localstack/localstack"))
+            new LocalStackContainer(DockerImageName.parse(getImageName()))
                     .withServices(DYNAMODB)
                     .withClasspathResourceMapping("testcontainers/init.sh",
                             "/docker-entrypoint-initaws.d/make-storages.sh", BindMode.READ_ONLY)
@@ -44,6 +44,6 @@ public class LocalStackTestConfig {
     }
 
     private static String getImageName() {
-        return System.getProperty("localstack_image_name", "public.ecr.aws/localstack/localstack:1.0.4");
+        return "localstack/localstack:1.0.4";
     }
 }

178e44c3589fdf15b8317f6a029a939167aff5dd
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-01-24 16:07:05 +0100

    PN-3390 modificato enrichBuilder

--- src/main/java/it/pagopa/pn/datavault/middleware/wsclient/common/BaseClient.java
+++ src/main/java/it/pagopa/pn/datavault/middleware/wsclient/common/BaseClient.java
@@ -1,17 +1,10 @@
 package it.pagopa.pn.datavault.middleware.wsclient.common;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 import it.pagopa.pn.commons.pnclients.CommonBaseClient;
 import it.pagopa.pn.datavault.generated.openapi.server.v1.dto.RecipientType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
 
-import java.time.Duration;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public abstract class BaseClient extends CommonBaseClient {
 
@@ -21,18 +14,8 @@ public abstract class BaseClient extends CommonBaseClient {
     }
 
     protected WebClient initWebClient(WebClient.Builder builder, String apiKey){
-        ConnectionProvider provider = ConnectionProvider.builder("fixed")
-                .maxConnections(500)
-                .maxIdleTime(Duration.ofSeconds(20))
-                .maxLifeTime(Duration.ofSeconds(60))
-                .pendingAcquireTimeout(Duration.ofSeconds(60))
-                .evictInBackground(Duration.ofSeconds(120)).build();
-
-        HttpClient httpClient = HttpClient.create(provider).option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-                .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS)));
 
         return super.enrichBuilder(builder)
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader(HEADER_API_KEY, apiKey)
                 .build();
     }

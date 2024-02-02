f8b8d4e9e3eb7eeaf60b54c6734df31807bd7ce9
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2024-02-02 17:55:09 +0100

    adds getNotificationTimelines test

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -11,5 +11,20 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     --provisioned-throughput \
         ReadCapacityUnits=10,WriteCapacityUnits=5
 
+# array of ConfidentialObjects entity
+TABLE_NAME="ConfidentialObjects"
+data=(
+    '{"hashKey": {"S": "TIMELINE#IUN1"}, "sortKey": {"S": "el1"}, "taxId": {"S": "Descrizione 1"}, "denomination": {"S": "Nome"}}'
+    '{"hashKey": {"S": "TIMELINE#IUN1"}, "sortKey": {"S": "el2"}, "taxId": {"S": "Descrizione 1"}, "denomination": {"S": "Nome"}}'
+    '{"hashKey": {"S": "TIMELINE#IUN1"}, "sortKey": {"S": "el2"}, "taxId": {"S": "Descrizione 1"}, "denomination": {"S": "Nome"}}'
+    )
+
+# Loop for put item into DynamoDB
+for item in "${data[@]}"; do
+    aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+     dynamodb put-item \
+    --table-name "$TABLE_NAME" \
+    --item "$item"
+done
 
 echo "Initialization terminated"

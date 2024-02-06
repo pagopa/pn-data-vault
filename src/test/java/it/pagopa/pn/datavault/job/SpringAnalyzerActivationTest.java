6c5c2511c3e57147c9d331f2e05e6807d8740ce8
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-02-06 15:30:30 +0100

    PN-9460: Spring Analyzer added

--- src/test/java/it/pagopa/pn/datavault/job/SpringAnalyzerActivationTest.java
+++ src/test/java/it/pagopa/pn/datavault/job/SpringAnalyzerActivationTest.java
@@ -3,36 +3,29 @@ package it.pagopa.pn.datavault.job;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
-import it.pagopa.pn.datavault.config.PnDatavaultConfig;
+import it.pagopa.pn.commons.utils.metrics.cloudwatch.CloudWatchMetricHandler;
+import it.pagopa.pn.datavault.springbootcfg.SpringAnalyzerActivation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
-import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
-import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataResponse;
 
-import java.util.concurrent.CompletableFuture;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class CloudWatchMetricJobTest {
+class SpringAnalyzerActivationTest {
 
     @Mock
     private RateLimiterRegistry rateLimiterRegistry;
 
-    @Mock
-    private CloudWatchAsyncClient cloudWatchAsyncClient;
-
-    @Mock
-    private PnDatavaultConfig pnDatavaultConfig;
-
     @InjectMocks
-    private CloudWatchMetricJob cloudWatchMetricJob;
+    private SpringAnalyzerActivation springAnalyzerActivation;
+
+    @Mock
+    private CloudWatchMetricHandler cloudWatchMetricHandler;
 
     @Test
     void testSendMetricToCloudWatch() {
@@ -40,10 +33,8 @@ class CloudWatchMetricJobTest {
         RateLimiter rateLimiterSELC = RateLimiter.of("selc-rate-limiter", RateLimiterConfig.custom().build());
         when(rateLimiterRegistry.rateLimiter("pdv-rate-limiter")).thenReturn(rateLimiterPDV);
         when(rateLimiterRegistry.rateLimiter("selc-rate-limiter")).thenReturn(rateLimiterSELC);
-        when(pnDatavaultConfig.getEnvRuntime()).thenReturn("test");
-        PutMetricDataResponse putMetricDataResponse = PutMetricDataResponse.builder().build();
-        when(cloudWatchAsyncClient.putMetricData(any(PutMetricDataRequest.class))).thenReturn(CompletableFuture.completedFuture(putMetricDataResponse));
+        when(cloudWatchMetricHandler.sendMetric(Mockito.anyString(), Mockito.any(), Mockito.anyString(), Mockito.anyDouble())).thenReturn(null);
 
-        Assertions.assertDoesNotThrow(() -> cloudWatchMetricJob.sendMetricToCloudWatch());
+        Assertions.assertDoesNotThrow(() -> springAnalyzerActivation.sendMetricToCloudWatch());
     }
 }
